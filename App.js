07caa12435b85ed776c0498b944b52c5bed5c7bb
kalyani mahajan <kalyanimahajan@kalyanis-MacBook-Pro.local>
2024-11-24 16:50:12 +0530

    React basic commit

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading= React.createElement("h1",{id:"heading"},"Helo React element");
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading); // this render method is 
+
+console.log(heading);
